e225974558903ee8939769f7bbd093fdd304223a
ndsch <ndschuring@gmail.com>
2024-10-29 14:40:16 +0100

    fix docstring

--- source/func/MacroscopicVariables.py
+++ source/func/MacroscopicVariables.py
@@ -3,11 +3,11 @@ import jax.numpy as jnp
 
 def get_density(f_i):
     """
-    Calculates local (macroscopic) densities from discrete velocities
+    Calculates local, macroscopic densities from discrete velocities
     Done by summing the discrete velocities at every grid point
 
     Args:
-        f_i (jnp.array):
+        f_i (jnp.array): array of discrete velocities
 
     Returns:
         jnp.array: array of local densities
@@ -17,14 +17,13 @@ def get_density(f_i):
 
 def get_velocity(f_i, force, c_i):
     """
-    Calculates local (macroscopic) velocity from discrete velocities
-    Done by summing the discrete velocities at every grid point
+    Calculates local, macroscopic velocity from discrete velocities
 
     Args:
-        f_i (jnp.array):
+        f_i (jnp.array): array of discrete velocities
 
     Returns:
-        jnp.array: array of local densities
+        jnp.array: array of local macroscopic velocities
     """
     rho = jnp.einsum('xyi->xy', f_i)
     u = jnp.einsum('ai,xyi->axy', c_i, f_i) + 0.5 * force
