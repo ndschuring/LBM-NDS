146a186b530e66df151baf4ee4d7dc4993e4cbf2
ndsch <ndschuring@gmail.com>
2024-10-29 11:25:35 +0100

    simulation parameters for single phase Poiseuille without incline+comments.

--- source/SimulationSetup/SimulationParametersD2Q9.py
+++ source/SimulationSetup/SimulationParametersD2Q9.py
@@ -15,26 +15,28 @@ scale = 1
 plot_every = 750 # 300 * scale
 skip_it = 5000 # 20000 * scale
 # Determine the size of the different regions
-r_0: int = int(30 * scale)
-r_1: int = int(50 * scale)
-r_2: int = int(100 * scale)
+# r_0: int = int(30 * scale)
+# r_1: int = int(50 * scale)
+# r_2: int = int(100 * scale)
 # Determine the simulation domain, NX is the channel length and NY is the channel height
-nx = r_0 + r_1 + r_2
-ny = int(30 * scale)
+nx = int(150 * scale)
+ny = int(50 * scale)
 nt = int(100000 * scale)
 # Simulation parameters
-kappa = 0.004
-rho_c = 3.5
-p_c = 0.125
-beta_tau = 0.03
-tau = 1
-u_max = 0.1 / scale
+kappa = 0.004                               #?
+rho_c = 3.5                                 # density bubble?
+p_c = 0.125                                 #?
+beta_tau = 0.03                             #?
+tau = 1                                     #relaxation time?
+u_max = 0.1 / scale                         # max velocity of some sort?
 Nu = (2 * tau - 1) / 6
-rho0 = rho_c * (1 - jnp.sqrt(beta_tau))
-Re = ny * u_max / Nu
+rho0 = rho_c * (1 - jnp.sqrt(beta_tau))     #background density?
+Re = ny * u_max / Nu                        #Simulation Reynold's number
 # Parameters related to gravity
-tilt_angle: float = 2 * math.pi * (60 / 360)
-g_set = 0.000003
+# tilt_angle: float = 2 * math.pi * (60 / 360)
+tilt_angle: float = 0.
+# g_set = 0.000003
+g_set = 0.
 # Parameters related to surface tension
 theta_1: float = 2 * math.pi * (150 / 360)  # contact angle we can set it in radians
 theta_2: float = 2 * math.pi * (80 / 360)  # contact angle we can set it in radians
