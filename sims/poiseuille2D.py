d14f20a408470180883826de1f4f0cf11e1f74ed
ndsch <ndschuring@gmail.com>
2024-11-20 09:51:11 +0100

    plots comparison to analytical solution. Fixed boundary condition application

--- sims/poiseuille2D.py
+++ sims/poiseuille2D.py
@@ -1,4 +1,5 @@
 from src.boundary_conditions import *
+import matplotlib.pyplot as plt
 from src.lattice import LatticeD2Q9
 from src.model import BGK
 import jax.numpy as jnp
@@ -24,17 +25,21 @@ class Poiseuille(BGK):
         super().__init__(**kwargs)
         # self.u_max = kwargs.get('u_max')
         # self.gradP = kwargs.get('gradP')
+        self.u_max = kwargs.get('u_max')
         self.rho_inlet = jnp.ones((self.nx, self.ny))*kwargs.get('rho_inlet')
         self.rho_outlet = jnp.ones((self.nx, self.ny))*kwargs.get('rho_outlet')
+        self.x = jnp.arange(1, self.nx+1) - 0.5
+        self.y = jnp.arange(1, self.ny+1) - 0.5
+        self.analytical_solution = self.poiseuille_analytical()
 
-    def apply_bc(self, f):
-        def bounce_back_tube2D(f_i):
+    def apply_bc(self, f, f_prev):
+        def bounce_back_tube2D(f_i, f_prev):
             # Bounce-back top wall
-            f_i = f_i.at[:, -1, self.lattice.bottom_indices].set(f_i[:, -1, self.lattice.top_indices])
+            f_i = f_i.at[:, -1, self.lattice.bottom_indices].set(f_prev[:, -1, self.lattice.top_indices])
             # Bounce-back bottom wall
-            f_i = f_i.at[:, -1, self.lattice.top_indices].set(f_i[:, -1, self.lattice.bottom_indices])
+            f_i = f_i.at[:, 0, self.lattice.top_indices].set(f_prev[:, 0, self.lattice.bottom_indices])
             return f_i
-        f = bounce_back_tube2D(f)
+        f = bounce_back_tube2D(f, f_prev)
         return f
 
     def apply_pre_bc(self, f_post_col, f_pre_col):
@@ -51,6 +56,30 @@ class Poiseuille(BGK):
         f_post_col = outlet_pressure(f_post_col)
         return f_post_col
 
+    def poiseuille_analytical(self):
+        ybottom = 0
+        ytop = self.ny
+        u_analytical = -4 * self.u_max / (self.ny ** 2) * (self.y - ybottom) * (self.y - ytop)
+        return u_analytical
+
+    def plot(self, f, it):
+        rho, u = self.macro_vars(f)
+        u_magnitude = jnp.linalg.norm(u, axis=-1, ord=2)
+        plt.imshow(u[:,:,0].T, cmap='viridis')
+        plt.imshow(u_magnitude.T, cmap='viridis')
+        plt.gca().invert_yaxis()
+        plt.colorbar()
+        plt.title("it:" + str(it) + "sum_rho:" + str(jnp.sum(rho)))
+        plt.savefig(self.sav_dir + "/fig_2D_it" + str(it) + ".jpg")
+        plt.clf()
+        plt.plot(self.y, u[int(self.nx/2),:,0], label="Poiseuille2D.py")
+        plt.plot(self.y, self.analytical_solution, label="analytical solution")
+        plt.plot(self.y, self.analytical_solution-u[int(self.nx/2),:,0], label="difference")
+        plt.ylim(0, self.u_max)
+        plt.legend()
+        plt.savefig(self.sav_dir + "/fig_1D_it" + str(it) + ".jpg")
+        plt.clf()
+
 
 if __name__ == "__main__":
     time1 = time.time()
@@ -61,8 +90,9 @@ if __name__ == "__main__":
     # tau = 1
     lattice = LatticeD2Q9()
 
-    tau = (3/16)**0.5 + 0.5             #relaxation time
-    nu = (2*tau-1)/6                    #kinematic shear velocity
+    # tau = jnp.sqrt(3/16) + 0.5             #relaxation time
+    tau = 1
+    nu = (2 * tau - 1) / 6                    #kinematic shear velocity
     u_max = 0.1                         #maximum velocity
     gradP = 8 * nu * u_max / ny ** 2    #pressure gradient
     rho_outlet = rho0
