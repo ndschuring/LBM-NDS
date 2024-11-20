c6d0c3427cc906e590a2f6b60f5b6275c22ffbd4
ndsch <ndschuring@gmail.com>
2024-11-20 09:50:12 +0100

    alternate version of poiseuille2D.py, boundary conditions are applied in a different order.

---
+++ sims/poiseuille2D_alt.py
@@ -0,0 +1,111 @@
+from src.boundary_conditions import *
+import matplotlib.pyplot as plt
+from src.lattice import LatticeD2Q9
+from src.model import BGK
+import jax.numpy as jnp
+import time
+"""
+Poiseuille Flow
+
+                        No slip BC
+        +-------------------------------------------+
+        |                                           |
+Periodic|                                           |Periodic
+Pressure|       ------------>                       |pressure
+   BC   |                                           |   BC
+        |                                           |
+        +-------------------------------------------+
+                        No slip BC
+"""
+
+
+
+class Poiseuille(BGK):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.u_max = kwargs.get('u_max')
+        # self.gradP = kwargs.get('gradP')
+        self.u_max = kwargs.get('u_max')
+        self.rho_inlet = jnp.ones((self.nx, self.ny))*kwargs.get('rho_inlet')
+        self.rho_outlet = jnp.ones((self.nx, self.ny))*kwargs.get('rho_outlet')
+        self.x = jnp.arange(1, self.nx+1) - 0.5
+        self.y = jnp.arange(1, self.ny+1) - 0.5
+        self.analytical_solution = self.poiseuille_analytical()
+
+    def apply_bc(self, f, f_prev):
+        def bounce_back_tube2D(f_i):
+            # Bounce-back top wall
+            f_i = f_i.at[:, -1, self.lattice.bottom_indices].set(f_prev[:, -1, self.lattice.top_indices])
+            # Bounce-back bottom wall
+            f_i = f_i.at[:, 0, self.lattice.top_indices].set(f_prev[:, 0, self.lattice.bottom_indices])
+            return f_i
+        def inlet_pressure(f_i):
+            f_i = f_i.at[0, :, :].set(self.equilibrium(self.rho_inlet, u_pre)[-2,:,:]+f_i[-2, :, :]-f_eq[-2,:,:])
+            return f_i
+        def outlet_pressure(f_i):
+            f_i = f_i.at[-1, :, :].set(self.equilibrium(self.rho_outlet, u_pre)[2, :, :] + f_i[2, :, :]-f_eq[2, :, :])
+            return f_i
+        rho_pre, u_pre = self.macro_vars(f_prev)
+        f_eq = self.equilibrium(rho_pre, u_pre)
+        f = inlet_pressure(f)
+        f = outlet_pressure(f)
+        f = bounce_back_tube2D(f)
+        return f
+
+    def poiseuille_analytical(self):
+        ybottom = 0
+        ytop = self.ny
+        u_analytical = -4 * self.u_max / (self.ny ** 2) * (self.y - ybottom) * (self.y - ytop)
+        return u_analytical
+
+    def plot(self, f, it):
+        rho, u = self.macro_vars(f)
+        u_magnitude = jnp.linalg.norm(u, axis=-1, ord=2)
+        plt.imshow(u[:,:,0].T, cmap='viridis')
+        plt.imshow(u_magnitude.T, cmap='viridis')
+        plt.gca().invert_yaxis()
+        plt.colorbar()
+        plt.title("it:" + str(it) + "sum_rho:" + str(jnp.sum(rho)))
+        plt.savefig(self.sav_dir + "/fig_2D_it" + str(it) + ".jpg")
+        plt.clf()
+        plt.plot(self.y, u[int(self.nx/2),:,0], label="Poiseuille2D.py")
+        plt.plot(self.y, self.analytical_solution, label="analytical solution")
+        plt.plot(self.y, self.analytical_solution-u[int(self.nx/2),:,0], label="difference")
+        plt.ylim(0, self.u_max)
+        plt.legend()
+        plt.savefig(self.sav_dir + "/fig_1D_it" + str(it) + ".jpg")
+        plt.clf()
+
+
+if __name__ == "__main__":
+    time1 = time.time()
+    nx = 180
+    ny = 30
+    nt = int(1e4)
+    rho0 = 1
+    # tau = 1
+    lattice = LatticeD2Q9()
+
+    # tau = jnp.sqrt(3/16) + 0.5             #relaxation time
+    tau = 1
+    nu = (2 * tau - 1) / 6                    #kinematic shear velocity
+    u_max = 0.1                         #maximum velocity
+    gradP = 8 * nu * u_max / ny ** 2    #pressure gradient
+    rho_outlet = rho0
+    rho_inlet = 3 * (nx - 1) * gradP + rho_outlet
+
+    kwargs = {
+        'lattice': lattice,
+        'tau': tau,
+        'nx': nx,
+        'ny': ny,
+        'rho0': rho0,
+        'u_max': u_max,
+        'gradP': gradP,
+        'rho_outlet': rho_outlet,
+        'rho_inlet': rho_inlet,
+    }
+    simPoiseuille = Poiseuille(**kwargs)
+    simPoiseuille.run(nt)
+    time2 = time.time()
+    print(time2-time1)
