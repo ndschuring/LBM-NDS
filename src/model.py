ba1a5b8c884577d34c7d1b32cfb5c9ee1f6fdda5
ndsch <ndschuring@gmail.com>
2024-11-20 14:21:42 +0100

    Optional source term added for force simulation

--- src/model.py
+++ src/model.py
@@ -7,13 +7,13 @@ class BGK(LBM):
         self.tau = kwargs.get("tau")  # relaxation factor tau
 
     @partial(jit, static_argnums=(0,), donate_argnums=(1,))
-    def collision(self, f):
+    def collision(self, f, source=0):
         """
         BGK Collision
         """
         rho, u = self.macro_vars(f)
         f_eq = self.equilibrium(rho, u)
-        return (1 - 1 / self.tau) * f + (1 / self.tau) * f_eq
+        return (1 - 1 / self.tau) * f + (1 / self.tau) * f_eq + (1 - 1 / (2 * self.tau)) * source
 
     # def collision(self, f):
     #     rho, u = self.macro_vars(f)
