bddb9dfb570282cf58027683721d713efdb1c2c2
ndsch <ndschuring@gmail.com>
2024-11-21 20:23:16 +0100

    added MRT skeleton

--- src/model.py
+++ src/model.py
@@ -7,20 +7,20 @@ class BGK(LBM):
         self.tau = kwargs.get("tau")  # relaxation factor tau
 
     @partial(jit, static_argnums=(0,), donate_argnums=(1,))
-    def collision(self, f, source=0):
+    def collision(self, f, source=0, force=None):
         """
         BGK Collision
         """
-        rho, u = self.macro_vars(f)
+        rho, u = self.macro_vars(f, force)
         f_eq = self.equilibrium(rho, u)
         return (1 - 1 / self.tau) * f + (1 / self.tau) * f_eq + (1 - 1 / (2 * self.tau)) * source
         # return (1 - 1 / self.tau) * f + (1 / self.tau) * f_eq + source
 
-    # def collision(self, f):
-    #     rho, u = self.macro_vars(f)
-    #     f_eq = self.equilibrium(rho, u)
-    #     f_neq = f - f_eq
-    #     f_post_col = f - 1/self.tau * f_neq
-    #     # if self.force is not None:
-    #     #     f_post_col = self.apply_force(f_post_col, f_eq, rho, u)
-    #     return f_post_col
+class MRT(LBM):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tau1 = kwargs.get("tau1")
+        self.tau2 = kwargs.get("tau2")
+
+    def collision(self, f, source=0):
+        pass
