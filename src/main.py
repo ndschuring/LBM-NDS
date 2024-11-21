b54158f440f1a0afb0ae1177b4d68905ea2801f8
ndsch <ndschuring@gmail.com>
2024-11-21 10:24:29 +0100

    application of force via source term added

--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ import datetime
 from functools import partial
 from jax import jit
 
+#    6   2   5
+#      \ | /
+#    3 - 0 - 1
+#      / | \
+#    7   4   8
+
 class LBM:
     def __init__(self, **kwargs):
         self.nx = kwargs.get("nx") #x dimension
@@ -17,7 +23,10 @@ class LBM:
         self.boundary_conditions = kwargs.get("boundary_conditions")
         self.lattice = kwargs.get("lattice") #set lattice
         self.plot_every = kwargs.get("plot_every", 50)
-        # self.plotter = kwargs.get("plotter") #set plotter
+        self.g_set = kwargs.get("g_set", 0)
+        self.tilt_angle = kwargs.get("tilt_angle", 0)
+        self.x = jnp.arange(1, self.nx+1) - 0.5
+        self.y = jnp.arange(1, self.ny+1) - 0.5
 
         # defining the following lattice parameters isn't necessary, just refer using self.lattice.x
         # self.d = self.lattice.d #space dimensions number from lattice
@@ -77,7 +86,9 @@ class LBM:
             3. Stream discrete velocities
                 stream(f)
         """
-        f_post_col = self.collision(f_prev)
+        force_prev = self.force_term(f_prev)
+        source_prev = self.source_term(f_prev, force_prev)
+        f_post_col = self.collision(f_prev, source_prev)
         f_post_col = self.apply_pre_bc(f_post_col, f_prev)
         f_post_stream = self.stream(f_post_col)
         f_post_stream = self.apply_bc(f_post_stream, f_post_col)
@@ -98,7 +109,7 @@ class LBM:
         #TODO
         pass
 
-    def collision(self, f):
+    def collision(self, f, source):
         """
         --Specified in model class--
         Applies collision
@@ -111,6 +122,51 @@ class LBM:
         Applies boundary conditions after streaming
         """
         pass
+
+    def force_term(self, f):
+        rho, u = self.macro_vars(f)
+        force_g = - rho * self.g_set
+        force_parr = - force_g * jnp.sin(self.tilt_angle)
+        force_perp = force_g * jnp.cos(self.tilt_angle)
+        force_components = jnp.stack((force_parr, force_perp), axis=0)
+        return force_components
+
+    def apply_force(self, u, force):
+        return u*0.5*force
+
+    def source_term(self, u, force):
+        ux, uy = u[:,:,0], u[:,:,1]
+        fx, fy = force[0], force[1]
+        cx, cy = self.lattice.c[0], self.lattice.c[1]
+        source_ = jnp.zeros((self.nx, self.ny, 9))
+        source_ = source_.at[:, :, 0].set(self.lattice.w[0] * (3 * (cx[0] * fx + cy[0] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[0] * cx[0] * ux * fx + cy[0] * cx[0] * uy * fx + cx[0] * cy[
+                    0] * ux * fy + cy[0] * cy[0] * uy * fy)))
+        source_ = source_.at[:, :, 1].set(self.lattice.w[1] * (3 * (cx[1] * fx + cy[1] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[1] * cx[1] * ux * fx + cy[1] * cx[1] * uy * fx + cx[1] * cy[
+                    1] * ux * fy + cy[1] * cy[1] * uy * fy)))
+        source_ = source_.at[:, :, 2].set(self.lattice.w[2] * (3 * (cx[2] * fx + cy[2] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[2] * cx[2] * ux * fx + cy[2] * cx[2] * uy * fx + cx[2] * cy[
+                    2] * ux * fy + cy[2] * cy[2] * uy * fy)))
+        source_ = source_.at[:, :, 3].set(self.lattice.w[3] * (3 * (cx[3] * fx + cy[3] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[3] * cx[3] * ux * fx + cy[3] * cx[3] * uy * fx + cx[3] * cy[
+                    3] * ux * fy + cy[3] * cy[3] * uy * fy)))
+        source_ = source_.at[:, :, 4].set(self.lattice.w[4] * (3 * (cx[4] * fx + cy[4] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[4] * cx[4] * ux * fx + cy[4] * cx[4] * uy * fx + cx[4] * cy[
+                    4] * ux * fy + cy[4] * cy[4] * uy * fy)))
+        source_ = source_.at[:, :, 5].set(self.lattice.w[5] * (3 * (cx[5] * fx + cy[5] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[5] * cx[5] * ux * fx + cy[5] * cx[5] * uy * fx + cx[5] * cy[
+                    5] * ux * fy + cy[5] * cy[5] * uy * fy)))
+        source_ = source_.at[:, :, 6].set(self.lattice.w[6] * (3 * (cx[6] * fx + cy[6] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[6] * cx[6] * ux * fx + cy[6] * cx[6] * uy * fx + cx[6] * cy[
+                    6] * ux * fy + cy[6] * cy[6] * uy * fy)))
+        source_ = source_.at[:, :, 7].set(self.lattice.w[7] * (3 * (cx[7] * fx + cy[7] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[7] * cx[7] * ux * fx + cy[7] * cx[7] * uy * fx + cx[7] * cy[
+                    7] * ux * fy + cy[7] * cy[7] * uy * fy)))
+        source_ = source_.at[:, :, 8].set(self.lattice.w[8] * (3 * (cx[8] * fx + cy[8] * fy) - 3 * (ux * fx + uy * fy) +
+                                                    9 * (cx[8] * cx[8] * ux * fx + cy[8] * cx[8] * uy * fx + cx[8] * cy[
+                    8] * ux * fy + cy[8] * cy[8] * uy * fy)))
+        return source_
 
     def apply_pre_bc(self, f, f_prev):
         """
