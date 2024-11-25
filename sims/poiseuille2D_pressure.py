91a59bb301d92f9d7985a9ad62030b670278cc59
ndsch <ndschuring@gmail.com>
2024-11-25 11:28:47 +0100

    new sim: poiseuille with pressure force

---
+++ sims/poiseuille2D_pressure.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+from src.lattice import LatticeD2Q9
+from src.model import BGK
+import jax.numpy as jnp
+import time
+
+#TODO make this thing
+class Poiseuille(BGK):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __str__(self):
+        return "Poiseuille_pressure"
+
+    def apply_bc(self, f, f_prev):
+        def bb_tube2d(f_i):
+            # bounce-back left wall
+            f_i = f_i.at[0, :, 5].set(f_prev[0, -1, 7])
+            f_i = f_i.at[0, :, 1].set(f_prev[0, -1, 3])
+            f_i = f_i.at[0, :, 8].set(f_prev[0, -1, 6])
+            # bounce-back right wall
+            f_i = f_i.at[-1, :, 6].set(f_prev[-1, 0, 8])
+            f_i = f_i.at[-1, :, 3].set(f_prev[-1, 0, 1])
+            f_i = f_i.at[-1, :, 7].set(f_prev[-1, 0, 5])
+            return f_i
+        f = bb_tube2d(f)
+        return f
+
+    def plot(self, f, it):
+        rho, u = self.macro_vars(f)
+        u_magnitude = jnp.linalg.norm(u, axis=-1, ord=2)
+        plt.imshow(u[:,:,0].T, cmap='viridis')
+        plt.imshow(u_magnitude.T, cmap='viridis')
+        plt.gca().invert_yaxis()
+        plt.colorbar()
+        plt.title("it:" + str(it) + "sum_rho:" + str(jnp.sum(rho)))
+        plt.savefig(self.sav_dir + "/fig_2D_it" + str(it) + ".jpg")
+        plt.clf()
+        # plt.plot(self.x, u[:, int(self.nx / 2), 1], label="Velocity Profile")
+        # plt.legend()
+        # plt.savefig(self.sav_dir + "/fig_1D_it" + str(it) + ".jpg")
+        # plt.clf()
+
+if __name__ == "__main__":
+    time1 = time.time()
+    nx = 30
+    ny = 180
+    nt = int(1e4)
+    # nt = int(1e3)
+    rho0 = 1
+    tau = 1
+    lattice = LatticeD2Q9()
+    plot_every = 100
+    u_in = 0.1 #inlet velocity
+
+    kwargs = {
+        'lattice': lattice,
+        'tau': tau,
+        'nx': nx,
+        'ny': ny,
+        'rho0': rho0,
+        'plot_every': plot_every,
+    }
+    simPoiseuille = Poiseuille(**kwargs)
+    simPoiseuille.run(nt)
+    time2 = time.time()
+    print(time2-time1)
