57f6c89701388b01c4c6270bc8a7942bd09ef559
ndsch <ndschuring@gmail.com>
2024-11-26 10:20:39 +0100

    testing collision operators

--- src/model.py
+++ src/model.py
@@ -13,8 +13,10 @@ class BGK(LBM):
         """
         rho, u = self.macro_vars(f, force)
         f_eq = self.equilibrium(rho, u)
-        return (1 - 1 / self.tau) * f + (1 / self.tau) * f_eq + (1 - 1 / (2 * self.tau)) * source
+        # return (1 - 1 / self.tau) * f + (1 / self.tau) * f_eq + (1 - 1 / (2 * self.tau)) * source
         # return (1 - 1 / self.tau) * f + (1 / self.tau) * f_eq + source
+        # return f - 1/self.tau*(f - f_eq) + source
+        return f- 1/self.tau*(f-f_eq)+(1 - 1/2*self.tau)*source
 
 class MRT(LBM):
     def __init__(self, **kwargs):
@@ -22,5 +24,5 @@ class MRT(LBM):
         self.tau1 = kwargs.get("tau1")
         self.tau2 = kwargs.get("tau2")
 
-    def collision(self, f, source=0):
+    def collision(self, f, source=0, force=None):
         pass
