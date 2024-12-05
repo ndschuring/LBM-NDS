eb2287506ad4fe9603585d30bd6b5d77e4bf403c
ndsch <ndschuring@gmail.com>
2024-12-05 13:03:51 +0100

    Added D3Q27 lattice support

--- src/lattice.py
+++ src/lattice.py
@@ -13,22 +13,36 @@ class Lattice:
         self.main_indices = jnp.array(self.main_indices())
         self.right_indices = jnp.array(self.right_indices())
         self.left_indices = jnp.array(self.left_indices())
-        self.top_indices = jnp.array(self.top_indices())
-        self.bottom_indices = jnp.array(self.bottom_indices())
+        if self.d >= 2:
+            self.top_indices = jnp.array(self.top_indices())
+            self.bottom_indices = jnp.array(self.bottom_indices())
+        if self.d >= 3:
+            self.front_indices = jnp.array(self.front_indices())
+            self.back_indices = jnp.array(self.back_indices())
 
     def lattice_velocity(self):
         if self.name == "D2Q9":
-            ci = jnp.array([[0, 1, 0, -1, 0, 1, -1, -1, 1],  # Velocities x components
-                            [0, 0, 1, 0, -1, 1, 1, -1, -1]])  # Velocities y components
+            ci = jnp.array([[0, 1, 0,-1, 0, 1,-1,-1, 1],  # Velocities x components
+                            [0, 0, 1, 0,-1, 1, 1,-1,-1]])  # Velocities y components
+        elif self.name == "D3Q27":
+            ci = jnp.array([
+                [0, 1, -1, 0,  0, 0,  0, 1, -1, 1, -1, 0,  0,  1, -1,  1, -1,  0,  0, 1, -1,  1, -1,  1, -1, -1,  1],
+                [0, 0,  0, 1, -1, 0,  0, 1, -1, 0,  0, 1, -1, -1,  1,  0,  0,  1, -1, 1, -1,  1, -1, -1,  1,  1, -1],
+                [0, 0,  0, 0,  0, 1, -1, 0,  0, 1, -1, 1, -1,  0,  0, -1,  1, -1,  1, 1, -1, -1,  1,  1, -1,  1, -1]
+            ])
         else:
-            raise ValueError("This Lattice type is not supported. Supported:D2Q9")
+            raise ValueError("This Lattice type is not supported")
         return ci
 
     def lattice_weight(self):
         if self.name == "D2Q9":
             wi = jnp.array([4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36])
+        elif self.name == "D3Q27":
+            wi = jnp.array([8 / 27, 2 / 27, 2 / 27, 2 / 27, 2 / 27, 2 / 27, 2 / 27,
+                           1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54, 1 / 54,
+                           1 / 216, 1 / 216, 1 / 216, 1 / 216, 1 / 216, 1 / 216, 1 / 216, 1 / 216])
         else:
-            raise ValueError("This Lattice type is not supported. Supported:D2Q9")
+            raise ValueError("This Lattice type is not supported")
         return wi
 
     def opposite_indices(self):
@@ -59,6 +73,14 @@ class Lattice:
         c = self.c.T
         return jnp.where(c[:, 1] == -1)[0]
 
+    def front_indices(self):
+        c = self.c.T
+        return jnp.where(c[:, 2] == 1)[0]
+
+    def back_indices(self):
+        c = self.c.T
+        return jnp.where(c[:, 2] == -1)[0]
+
 
 class LatticeD2Q9(Lattice):
     def __init__(self):
@@ -70,3 +92,15 @@ class LatticeD2Q9(Lattice):
         self.cs2 = 1 / 3
         self.inv_cs2 = 3
         self.i = jnp.arange(self.q)
+
+class LatticeD3Q27(Lattice):
+    def __init__(self):
+        super().__init__("D3Q27")
+        self._set_constants()
+
+    def _set_constants(self):
+        self.cs = 1 / jnp.sqrt(3)
+        self.cs2 = 1 / 3
+        self.inv_cs2 = 3
+        self.i = jnp.arange(self.q)
+
